db6643b4d0fc5b0fd0f65347b4b357007c58cd6b
Andrew Metzgus <andrew@metzg.us>
2022-04-16 09:00:13 -0700

    Initial commit

---
+++ stackbuilders_lp/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-9mpr(pn4$ihn%xk+a&958lu&iwnrrpx#px-xo-j3kd2g&n1rw0'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
